7c0de80582fa522d66a3da457a747803108256b4
numind <stefanocamisassi@MacBook-Air-di-Stefano.local>
2016-12-07 01:03:00 +0100

    end of first part of the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>ciao come va!</h1>
-    <fa-lifecycle *ngIf="!delete"></fa-lifecycle>
+    <fa-lifecycle *ngIf="!delete" [bindable]="boundValue">
+    <p #boundContent>{{test}}</p>
+    </fa-lifecycle>
     <button (click)="delete = true">Click to delete</button>
+    <button (click)="test = 'changed value'">Click to change content</button>
+    <button (click)="boundValue = 2000">Click to change binding</button>
     `
 })
 
 export class AppComponent {
   title = 'app works! ... I chaged it!';
   delete = false;
+  test = 'Starting value';
+  boundValue = 1000;
 }
 
 // styleUrls: ['./app.component.css']
--- src/app/lifecycle.component.ts
+++ src/app/lifecycle.component.ts
@@ -1,24 +1,36 @@
-import { 
-    Component, 
-    OnInit, 
-    OnChanges, 
-    DoCheck, 
-    AfterContentInit, 
-    AfterContentChecked, 
+import {
+    Component,
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
     AfterViewInit,
     AfterViewChecked,
-    OnDestroy } from '@angular/core';
+    OnDestroy,
+    Input,
+    ViewChild,
+  ContentChild } from '@angular/core';
 
 @Component({
   selector: 'fa-lifecycle',
   template: `
-    <p>
-      lifecycle Works!
-    </p>
+    <ng-content></ng-content>
+    <!-- Local Variable in the template -->
+    <p #boundParagraph>{{bindable}}</p>
+    <p>{{boundParagraph.textContent}}</p>
+
   `,
   styleUrls: ['./lifecycle.component.css']
 })
 export class LifecycleComponent implements OnChanges, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  @Input() bindable = 1000;
+  @ViewChild('boundParagraph')
+  boundParagraph: HTMLElement;
+
+  @ContentChild('boundContent')
+  boundContent: HTMLElement;
 
   constructor() { }
 
@@ -34,12 +46,14 @@ export class LifecycleComponent implements OnChanges, OnInit, OnChanges, DoCheck
   }
   ngAfterContentInit() {
     this.log('ngAfterContentInit');
+    console.log(this.boundContent);
   }
   ngAfterContentChecked() {
     this.log('ngAfterContentChecked');
   }
   ngAfterViewInit() {
     this.log('ngAfterViewInit');
+    console.log(this.boundParagraph);
   }
   ngAfterViewChecked() {
     this.log('ngAfterViewChecked');
